cd72c6efa899e18f7295f9b5ad0de6575fa7bb41
Mateus Henrique <mateushbsb@gmail.com>
2023-05-09 16:03:15 -0300

    Implementation service

--- interfaceSoluSem/src/application/Program.java
+++ interfaceSoluSem/src/application/Program.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 
 import model.entities.CarRental;
 import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
 
 public class Program {
 
@@ -26,6 +28,20 @@ public class Program {
 		
 				
 		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		System.out.println("Entre com o preço por hora:");
+		double pricePerHour = sc.nextDouble();
+		System.out.println("Entre com o preço por Dia:");
+		double pricePerDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+		
+		rentalService.processInvoice(cr);
+		
+		System.out.println("Fatura:");
+		System.out.println("Pagamento basico: " + String.format( "%.2f", cr.getInvoice().getBasicPayment()));
+		System.out.println("Imposto: " + String.format( "%.2f",cr.getInvoice().getTax()));
+		System.out.println("Pagamento total: " + String.format( "%.2f",cr.getInvoice().getTotalPayment()));
 		
 		sc.close();
 	}
